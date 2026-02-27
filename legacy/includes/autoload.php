79b4ca2d73c3f7e5724a618447acd44179a82ca9
George Violaris <george@Georges-MacBook-Pro-2.local>
2026-02-27 12:28:12 +0200

    Remove legacy code and planning docs
    Legacy folder contained pre-upgrade files that are fully replaced
    by the new framework. Plans folder served its purpose during
    implementation.

--- legacy/includes/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($className) {
-    $file = dirname(__DIR__) . '\\' . $className . '.php';
-    $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
-    echo $file;
-    if (file_exists($file)) {
-        include $file;
-    }
-});
